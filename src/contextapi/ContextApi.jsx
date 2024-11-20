ae3dc897ff3721c1828902754ee11d5595c0704a
mdibrahimofc <mdibrahim.ofc@gmail.com>
2024-11-20 19:36:43 +0600

    add login and register route

--- src/contextapi/ContextApi.jsx
+++ src/contextapi/ContextApi.jsx
@@ -1,15 +1,40 @@
-import React, { Children, createContext } from 'react';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { Children, createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import { auth } from '../firebase/firebase.config';
+
 export const AuthContext = createContext(null)
 const ContextApi = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        onAuthStateChanged(auth, (currentUser) => {
+            if(user){
+                setUser(currentUser)
+                setLoading(false)
+            }
+            else{
+                setUser(null)
+            }
+        })
+    }, [])
 
     const createAccount = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const googleProvider = new GoogleAuthProvider()
+
+    const signInGoogle = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
+
     const AuthInfo = {
-        createAccount
+        createAccount,
+        loading,
+        user,
+        signInGoogle,
     }
     return (
         <AuthContext.Provider value={AuthInfo}>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../contextapi/ContextApi";
-import loginImg from "../assets/original-0c14504bd291054d76548cb015dff89a.png"
+import loginImg from "../assets/original-0c14504bd291054d76548cb015dff89a.png";
+import { Link } from "react-router-dom";
+import { FaRegEye, FaEyeSlash } from "react-icons/fa";
+import swal from 'sweetalert';
 
 const Register = () => {
-  const {createAccount} = useContext(AuthContext)
-  console.log(createAccount);
-    const handleRegister = () => {
-        
+  const [showPassword, setShowPassword] = useState(false);
+  const [uppercase, setUppercase] = useState(false)
+  const { createAccount } = useContext(AuthContext);
+  const [password, setPassword] = useState('')
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const { name, photo, checkbox, email, password } = e.target;
+    if (!checkbox.checked) {
+      swal("You must accept our terms and condition to procceed");
+      return;
     }
+    createAccount(email.value, password.value).then((res) => {
+      console.log(res.user);
+    });
+  };
+  const handleChange = (e) => {
+    const passwordValue = e.target.value
+    console.log(passwordValue);
+    setPassword(passwordValue)
+    console.log(password);
+  }
+  const handlePasswordValidate = () => {
+    const regexU = /^(?=.*[A-Z]).+$/; // Regex for at least one uppercase letter
+    if (regexU.test(password)) { // Use regex.test() to check the password
+      setUppercase(true);
+    } else {
+      setUppercase(false);
+    }
+  };
+  
   return (
     <div className="flex h-screen bg-[#E9E9E9]">
       <div className="hidden md:flex w-1/2 justify-center items-center">
-        <img
-          src={loginImg}
-          alt="Graphic"
-          className="object-cover"
-        />
+        <img src={loginImg} alt="Graphic" className="object-cover" />
       </div>
 
       <div className="flex flex-col justify-center md:shadow-md md:rounded-2xl items-center w-full md:w-1/2 bg-white px-8">
@@ -27,53 +51,91 @@ const Register = () => {
             Please enter your details
           </p>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleRegister}>
             <div>
-              <label className="block text-gray-700 font-medium mb-1">Email</label>
+              <label className="block text-gray-700 font-medium mb-1">
+                User name
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter your user name"
+                required
+                className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-black"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                PhotoURL
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Enter your photo url"
+                required
+                className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-black"
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-medium mb-1">
+                Email
+              </label>
               <input
                 type="email"
+                name="email"
                 placeholder="Enter your email"
+                required
                 className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-black"
               />
             </div>
 
             <div>
-              <label className="block text-gray-700 font-medium mb-1">Password</label>
+              <label className="block text-gray-700 font-medium mb-1">
+                Password
+              </label>
               <div className="relative">
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  onClick={handlePasswordValidate}
+                  onChange={handleChange}
                   placeholder="Enter your password"
+                  required
                   className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-black"
                 />
-                <span className="absolute right-3 top-3 text-gray-500 cursor-pointer">
-                  👁️
+                <span
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-3 text-xl text-gray-500 cursor-pointer"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaRegEye />}
                 </span>
               </div>
+              {
+                password && <div>
+                  <p className={uppercase && 'text-green-600'}>Must have an Uppercase letter in the password</p>
+                </div>
+              }
             </div>
 
-            <div className="flex items-center justify-between text-sm">
-              <a href="#" className="text-gray-600 hover:underline">
-                Forgot password?
-              </a>
+            <div>
+              <label className="flex items-center">
+                <input type="checkbox" name="checkbox" className="mr-2" />I
+                agree to the{" "}
+                <span className="text-green-500 ml-1">
+                  Terms and Conditions
+                </span>
+                .
+              </label>
             </div>
 
-            <button className="btn btn-black w-full">Log In</button>
-
-            <button className="btn btn-outline btn-primary w-full mt-2">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png"
-                alt="Google Logo"
-                className="w-5 h-5 mr-2"
-              />
-              Log in with Google
-            </button>
+            <button className="btn btn-primary w-full">Sign Up</button>
           </form>
 
           <p className="text-center text-sm text-gray-600 mt-4">
-            Don’t have an account?{" "}
-            <a href="#" className="text-primary hover:underline">
-              Sign up
-            </a>
+            Already have an account?{" "}
+            <Link to="/login" className="text-primary hover:underline">
+              Sign Up
+            </Link>
           </p>
         </div>
       </div>
